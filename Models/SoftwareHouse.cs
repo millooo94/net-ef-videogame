39263e859db7b39057153d22cc057b0ffa7e73b3
millooo94 <camillomusmeciunict@gmail.com>
2023-03-31 13:36:26 +0200

    Aggiungere i file di progetto.

---
+++ Models/SoftwareHouse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace net_ef_videogame.Models
+{
+    public class SoftwareHouse
+    {
+    }
+}
